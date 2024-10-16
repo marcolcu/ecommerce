93e981c8fcbbf14506c791fc63307f932c41de8e
marcolcu <marcomelandri808@gmail.com>
2024-10-16 21:34:20 +0700

    [UPDATE]

--- src/components/component/banner.tsx
+++ src/components/component/banner.tsx
@@ -39,7 +39,7 @@ const Banner: React.FC = () => {
                         <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent" />
                         <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
                             <h3 className="text-2xl font-bold">Summer Sale is on!</h3>
-                            <p className="mt-2 text-lg">Don't miss out on our amazing discounts.</p>
+                            <p className="mt-2 text-lg">Don&apos;t miss out on our amazing discounts.</p>
                             <Button className="mt-4">Shop Now</Button>
                         </div>
                     </div>
